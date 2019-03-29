dedd0daf21a900770d14166ab6e61df0b2d031a7
Petr Vích <vichpetr@gmail.com>
2019-03-29 20:01:25 +0100

    create new dist version

--- SnackBar/src/environments/environment.prod.ts
+++ SnackBar/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://api.snackbar.petrvich.eu',
+  url: 'http://snack-bar-java-sandbox-arche-innovation.telitcaas1.t-internal.com',
   localAddress: '172.16.9.19:8080',
-  publicAddress: 'api.snackbar.petrvich.eu'
+  publicAddress: 'snack-bar-java-sandbox-arche-innovation.telitcaas1.t-internal.com'
 };
